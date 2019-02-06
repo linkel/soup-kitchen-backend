4729eee9b634f9969dde0e9392580a002023a283
Chaya Otikor <c.otikor@gmail.com>
2019-02-05 18:03:23 -0700

    Hard coding frontend user, cors fix

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,44 +1,48 @@
-const faker = require('faker')
+const faker = require('faker');
 
 const createFakeStaff = () => ({
-  firstname: faker.name.firstName(),
-  lastname: faker.name.lastName(),
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-  role: 'staff'
-})
+	firstname: faker.name.firstName(),
+	lastname: faker.name.lastName(),
+	email: faker.internet.email(),
+	password: faker.internet.password(),
+	role: 'staff'
+});
 const createFakeVolunteers = () => ({
-  firstname: faker.name.firstName(),
-  lastname: faker.name.lastName(),
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-  role: 'volunteer'
-})
+	firstname: faker.name.firstName(),
+	lastname: faker.name.lastName(),
+	email: faker.internet.email(),
+	password: faker.internet.password(),
+	role: 'volunteer'
+});
 const createFakeAdmin = () => ({
-  firstname: faker.name.firstName(),
-  lastname: faker.name.lastName(),
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-  role: 'admin'
-})
-
+	firstname: faker.name.firstName(),
+	lastname: faker.name.lastName(),
+	email: faker.internet.email(),
+	password: faker.internet.password(),
+	role: 'admin'
+});
 
 exports.seed = async function(knex, Promise) {
-  const fakeUsers = []
-  const desiredStaff = 10;
-  const desiredAdmins = 5;
-  const desiredVolunteers = 15;
+	const fakeUsers = [
+		{
+			email: 'kelly@lin.com',
+			password: 'test',
+			firstname: 'Kelly',
+			lastname: 'Lin'
+		}
+	];
+	const desiredStaff = 10;
+	const desiredAdmins = 5;
+	const desiredVolunteers = 15;
 
-  for(let i = 0; i < desiredAdmins; i++){
-    fakeUsers.push(createFakeAdmin())
-  }
-  for(let i = 0; i < desiredStaff; i++){
-    fakeUsers.push(createFakeStaff())
-  }
-  for(let i = 0; i < desiredVolunteers; i++){
-    fakeUsers.push(createFakeVolunteers())
-  }
-  await knex("users")
-  .insert(fakeUsers)
- 
+	for (let i = 0; i < desiredAdmins; i++) {
+		fakeUsers.push(createFakeAdmin());
+	}
+	for (let i = 0; i < desiredStaff; i++) {
+		fakeUsers.push(createFakeStaff());
+	}
+	for (let i = 0; i < desiredVolunteers; i++) {
+		fakeUsers.push(createFakeVolunteers());
+	}
+	await knex('users').insert(fakeUsers);
 };
--- middleware/globalMiddleware.js
+++ middleware/globalMiddleware.js
@@ -9,5 +9,5 @@ module.exports = (server) => {
 	server.use(helmet());
 	server.use(express.json());
 	server.use(morgan('short'));
-	server.use(cors({ credentials: true, origin: 'https://soup-kitchen.netlify.com/' }));
+	server.use(cors());
 };
